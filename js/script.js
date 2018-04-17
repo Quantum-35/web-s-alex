fb9c5f684f36ed7d7070e336b580fc2d9dd1f67a
Sam Ngugi <ngugindunguapps@gmail.com>
2018-04-17 07:25:50 +0300

    Fix margins and font-sizes

--- js/script.js
+++ js/script.js
@@ -2,23 +2,23 @@ $(document).ready(function(){
     $('#buttonFeatures').click(function () {
         $('html, body').animate({
             scrollTop: $("#features").offset().top
-        }, 1000);
+        }, 2500);
     });
     $('#buttonContact').click(function () {
         $('html, body').animate({
             scrollTop: $("#contact").offset().top
-        }, 1000);
+        }, 2500);
     });
 
      $('#aboutButton').click(function () {
          $('html, body').animate({
              scrollTop: $("#about").offset().top
-         }, 1000);
+         }, 2500);
      });
     $('#buttonGetStarted').click(function () {
           $('html, body').animate({
               scrollTop: $("#div-getstarted").offset().top
-          }, 1000);
+          }, 2500);
       });
     $('#btnTakeMeHome').click(function () {
         $('html, body').animate({ scrollTop: 0 }, 1000);
