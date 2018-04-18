8bb94d66298cf6ab885877057c782a1dcaa36e58
Sam Ngugi😀 <ngugindunguapps@gmail.com>
2018-04-18 04:16:00 +0300

    Add email verification

--- js/script.js
+++ js/script.js
@@ -23,4 +23,38 @@ $(document).ready(function(){
     $('#btnTakeMeHome').click(function () {
         $('html, body').animate({ scrollTop: 0 }, 1000);
     });
+   
+    function gup( name, url ) {
+        if (!url) url = location.href;
+        name = name.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
+        var regexS = "[\\?&]"+name+"=([^&#]*)";
+        var regex = new RegExp( regexS );
+        var results = regex.exec( url );
+        return results == null ? null : results[1];
+    }
+    var code = gup('code',window.location.search);
+    if(code != null) {
+        $('.someBlock').preloader({
+            text: 'Verifying email',
+            percent: '',
+            duration: '1000000000'
+        });
+        var params = {
+            code: code
+        };
+        $.ajax({
+            type: 'PUT',
+            url: 'http://localhost:8000/v0/email-verification/verify',
+            date: params
+        })
+        .done(function() {
+            $('#success').text('Email verified successfully');
+          })
+          .fail(function(error) {
+            $('#error').text('Error occoured while verifying your email, please try again later');
+          })
+          .always(function(){
+              $('.someBlock').preloader('remove');
+          })
+    }
 });
